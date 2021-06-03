e8d8d4a51be014bceaf932c02834e1e0ca8c0c9b
iarpittomar <arpittomar7740@gmail.com>
2021-06-04 00:04:59 +0530

    feat: setup jest and write unit test casees for sync and async code

---
+++ script-2.test.js
@@ -0,0 +1,50 @@
+const fetch = require("node-fetch");
+const swapi = require("./script-2");
+
+// it("calls swapi to get people", (done) => {
+//   expect.assertions(1);
+//   swapi.getPeople(fetch).then((data) => {
+//     expect(data.count).toEqual(87);
+//     done();
+//   });
+// });
+
+it("calls swapi to get people", () => {
+  expect.assertions(1);
+  return swapi.getPeople(fetch).then((data) => {
+    expect(data.count).toEqual(87);
+  });
+});
+
+it("calls swapi to get people with a promise", () => {
+  expect.assertions(2);
+
+  return swapi.getPeoplePromise(fetch).then((data) => {
+    expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(5);
+  });
+});
+
+/* if we have long running database call or api call
+then to avoid those we can create a mock functions. We also call mock functions as spies in JEST */
+//Because mocks let us spy on the behaviour of functions
+it("getPeople returnscount and results", () => {
+  const mockFetch = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          count: 87,
+          results: [0, 1, 2, 3, 4, 5],
+        }),
+    })
+  );
+
+  expect.assertions(4);
+
+  return swapi.getPeoplePromise(mockFetch).then((data) => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith("https://swapi.py4e.com/api/people");
+    expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(5);
+  });
+});
